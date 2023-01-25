7458076207907948962c2f46bdac87d8e186fa59
Tronje-the-Falconer <club@kording-web.de>
2023-01-25 14:01:48 +0100

    Update index.php

--- var/www/index.php
+++ var/www/index.php
@@ -1,1 +1,16 @@
-init-file
+echo '<form  method="post" name="boot">';
+    echo '<table style="width: 100%;">';
+      echo '<tr>';
+        echo '<td>weight in kg: </td>';
+        echo '<td><input type="number" style="width: 35%;" min="0" max="200" name="weight"></td>';
+      echo '</tr></tr>';
+        echo '<td>bodyfat in %: </td>';
+        echo '<td><input type="number" style="width: 35%;" min="0" max="200" name="bodyfat"></td>';
+      echo '</tr>';
+  echo '</table>';
+echo '</form>';
+
+echo'<button class="art-button" name="synchronize"  value="synchronize" onclick="return confirm("attention: sync all values");">synchronize</button>';
+
+
+shell_exec('sudo /var/sudowebscript.sh run_healthsync  > /dev/null 2>&1 &');
