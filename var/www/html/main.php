7b6c7c2b30f794c89b210fbefd8b36702d74cbf4
Tronje-the-Falconer <club@kording-web.de>
2023-02-06 08:39:36 +0100

    Add files via upload

--- var/www/html/main.php
+++ var/www/html/main.php
@@ -8,16 +8,41 @@
     <style>
         body {
           font-size: 20px;
+          align: center;
+        }
+        
+        .button {
+          display: inline-block;
+          padding: 15px 25px;
+          font-size: 24px;
+          cursor: pointer;
+          text-align: center;
+          text-decoration: none;
+          outline: none;
+          color: #fff;
+          background-color: #4CAF50;
+          border: none;
+          border-radius: 15px;
+          box-shadow: 0 9px #999;
+        }
+
+        .button:hover {background-color: #3e8e41}
+
+        .button:active {
+          background-color: #3e8e41;
+          box-shadow: 0 5px #666;
+          transform: translateY(4px);
         }
     </style>
     <body>
         <h1>health synchronizing </h1>
+        <h2> todays values </h2>
         <form  action="/synchronize.php" method="post" name="healthsata">
             <table>
                 <tr>
                     <td>fatigue (pre training): </td>
                     <td>
-                        <input type="radio" id="fatigue1" value=1 name="fatigue">
+                        <input type="radio" id="fatigue1" value=1 name="fatigue" checked>
                             <label for="fatigue1" >Low</label>
                         <input type="radio" id="fatigue2" value=2 name="fatigue">
                             <label for="fatigue2" >Avg</label>
@@ -27,9 +52,42 @@
                             <label for="fatigue4" >Extreme</label>
                     </td>
                 </tr></tr>
+                <td>mood: </td>
+                    <td>
+                        <input type="radio" id="mood1" value=1 name="mood">
+                            <label for="mood1" >&#129321; Excellent</label>
+                        <input type="radio" id="mood2" value=2 name="mood" checked>
+                            <label for="mood2" >&#128526; Good</label>
+                        <input type="radio" id="mood3" value=3 name="mood">
+                            <label for="mood3" >&#128528; Fair</label>
+                        <input type="radio" id="mood4" value=4 name="mood">
+                            <label for="mood4" >&#128532; Poor</label>
+                    </td>
+                </tr></tr>
+                    <td>weight (kg): </td>
+                        <td><input type="number" step="0.1" style="width: 35%;" min="0.0" max="200" name="weight"></td>
+                    </tr></tr>
+                        <td>bodyfat (%): </td>
+                        <td><input type="number" step="0.1" style="width: 35%;" min="0" max="20" name="bodyfat"></td>
+                    </tr></tr>
+                        <td>temperature (°C): </td>
+                        <td><input type="number" step="0.1" style="width: 35%;" min="0" max="50" name="temperature"></td>
+                    </tr></tr>
+                        <td>systolic: </td>
+                        <td><input type="number" style="width: 35%;" min="0" max="500" name="systolic"></td>
+                    </tr></tr>
+                        <td>diastolic: </td>
+                        <td><input type="number" style="width: 35%;" min="0" max="500" name="diastolic"></td>
+                </tr>
+                </table>
+                </br>
+                
+                <h2> yesterdays values </h2>
+                <table>
+                <tr>
                     <td>stress: </td>
                     <td>
-                        <input type="radio" id="stress1" value=1 name="stress">
+                        <input type="radio" id="stress1" value=1 name="stress" checked>
                             <label for="stress1" >Low</label>
                         <input type="radio" id="stress2" value=2 name="stress">
                             <label for="stress2" >Avg</label>
@@ -39,35 +97,21 @@
                             <label for="stress4" >Extreme</label>
                     </td>
                 </tr></tr>
-                    <td>mood: </td>
+                    <td>Alcohol: </td>
                     <td>
-                    <input type="radio" id="mood1" value=1 name="mood">
-                        <label for="mood1" >&#129321; Excellent</label>
-                    <input type="radio" id="mood2" value=2 name="mood">
-                        <label for="mood2" >&#128526; Good</label>
-                    <input type="radio" id="mood3" value=3 name="mood">
-                        <label for="mood3" >&#128528; Fair</label>
-                    <input type="radio" id="mood4" value=4 name="mood">
-                        <label for="mood4" >&#128532; Poor</label>
-                </td>
-                </tr></tr>
-                    <td>weight (kg): </td>
-                    <td><input type="number" step="0.1" style="width: 35%;" min="0.0" max="200" name="weight"></td>
-                </tr></tr>
-                    <td>bodyfat (%): </td>
-                    <td><input type="number" step="0.1" style="width: 35%;" min="0" max="20" name="bodyfat"></td>
-                </tr></tr>
-                    <td>temperature (°C): </td>
-                    <td><input type="number" step="0.1" style="width: 35%;" min="0" max="50" name="temperature"></td>
-                </tr></tr>
-                    <td>systolic: </td>
-                    <td><input type="number" style="width: 35%;" min="0" max="500" name="systolic"></td>
-                </tr></tr>
-                    <td>diastolic: </td>
-                    <td><input type="number" style="width: 35%;" min="0" max="500" name="diastolic"></td>
+                    <input type="radio" id="alcohol0" value=0 name="alcohol" checked>
+                        <label for="alcohol0" >None</label>
+                    <input type="radio" id="alcohol1" value=1 name="alcohol">
+                        <label for="alcohol1" >A little</label>
+                    <input type="radio" id="alcohol2" value=2 name="alcohol">
+                        <label for="alcohol2" >Average</label>
+                    <input type="radio" id="alcohol3" value=3 name="alcohol">
+                        <label for="alcohol3" >Excessive</label>
+                    </td>
                 </tr>
             </table>
-            <button class="art-button" name="synchronize"  value="synchronize" onclick="return confirm("attention: sync all values")>synchronize</button>
+            </br>
+            <button name="synchronize" class="button"  value="synchronize">synchronize</button>
         </form>
     </body>
 </html>
--- var/www/html/synchronize.php
+++ var/www/html/synchronize.php
@@ -3,51 +3,52 @@
     $date = date('d.m.Y H:i:s');
     $parameter = '"';
     $fatigue = $_POST['fatigue'];
-    if ($fatigue != '' AND $fatigue != 0 AND $fatigue != NULL){
+    if ($fatigue != '' AND $fatigue != NULL){
         $parameter .= ' --fatigue ' . $fatigue;
     }
     $stress = $_POST['stress'];
-    if ($stress != '' AND $stress != 0 AND $stress != NULL){
+    if ($stress != ''AND $stress != NULL){
         $parameter .= ' --stress ' . $stress;
     }
     $mood = $_POST['mood'];
-    if ($mood != '' AND $mood != 0 AND $mood != NULL){
+    if ($mood != '' AND $mood != NULL){
         $parameter .= ' --mood ' . $mood;
     }
     $weight = $_POST['weight'];
-    if ($weight != '' AND $weight != 0 AND $weight != NULL){
+    if ($weight != '' AND $weight != NULL){
         $parameter .= ' --weight ' . $weight;
     }
     $bodyfat = $_POST['bodyfat'];
-    if ($bodyfat != '' AND $bodyfat != 0 AND $bodyfat != NULL){
+    if ($bodyfat != '' AND $bodyfat != NULL){
         $parameter .= ' --bodyfat ' . $bodyfat;
     }
     $temperature = $_POST['temperature'];
-    if ($temperature != '' AND $temperature != 0 AND $temperature != NULL){
+    if ($temperature != '' AND $temperature != NULL){
         $parameter .= ' --temperature ' . $temperature;
     }
     $systolic = $_POST['systolic'];
-    if ($systolic != '' AND $systolic != 0 AND $systolic != NULL){
+    if ($systolic != '' AND $systolic != NULL){
         $parameter .= ' --systolic ' . $systolic;
     }
     $diastolic = $_POST['diastolic'];
-    if ($diastolic != '' AND $diastolic != 0 AND $diastolic != NULL){
+    if ($diastolic != '' AND $diastolic != NULL){
         $parameter .= ' --diastolic ' . $diastolic;
+    }
+    $alcohol = $_POST['alcohol'];
+    if ($alcohol != '' AND $alcohol != NULL){
+        $parameter .= ' --alcohol ' . $alcohol;
     }
     $parameter .= ' "';
 
     if (isset ($_POST['synchronize'])){
         unset($_POST['synchronize']);
-        $command = 'sudo /var/sudowebscript.sh run_healthsync ' . $parameter;
-        print "shell_exec(".$command.")";
+        shell_exec('sudo /var/sudowebscript.sh run_healthsync ' . $parameter );
         #show 30 seconds
         echo '<p id=\'info-message\' style=\'color: #ff0000; font-size: 20px;\'><b>synchronizing startet</b><br>' . $date . ' <br> values send: ' . $parameter .' </p>
                 <script language="javascript">
-                    setTimeout(function(){document.getElementById("info-message").style.display="none"}, 30000)
+                    setTimeout(function(){document.getElementById("info-message").style.display="none"}, 10000)
                 </script>';
-        shell_exec('sudo /var/sudowebscript.sh run_healthsync ' . $parameter );
-        
-        header("Refresh:30; url=main.php");
+        header("Refresh:15; url=main.php");
         die();
 
     }
